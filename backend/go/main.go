82f565393ff6f5289dbbf08ae1704919ece14b08
Drumil Bhati <64512394+drumilbhati@users.noreply.github.com>
2026-03-18 08:43:11 +0530

    feat: Initialize HTTP server with Gorilla Mux and add `/api/health` endpoint.

--- backend/go/main.go
+++ backend/go/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-    fmt.Println("Go Backend Service Started")
+	r := mux.NewRouter()
+
+	// Create a subrouter with /api prefix
+	api := r.PathPrefix("/api").Subrouter()
+
+	// Create an api to check health of server
+	api.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
+	// Listen on port 8080
+	http.ListenAndServe(":8080", r)
 }
